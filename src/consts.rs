d4c54dde83a5a3a5dcabdb441852ad3cbc0b7847
 Danil B <goosedb@MacBook-Pro-Danil.local>
2018-01-30 20:30:51 +0800

    //

---
+++ src/consts.rs
@@ -0,0 +1,2 @@
+pub const WIDTH: i32 = 100;
+pub const HEIGHT: i32 = 20;
---
+++ src/coord.rs
@@ -0,0 +1,35 @@
+use std::ops::{Add, Sub};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Coord {
+    pub x: i32,
+    pub y: i32,
+}
+
+impl Coord {
+    pub fn new(x: i32, y: i32) -> Coord {
+        Coord { x: x, y: y }
+    }
+}
+
+impl Add for Coord {
+    type Output = Coord;
+
+    fn add(self, other: Coord) -> Coord {
+        Coord {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Sub for Coord {
+    type Output = Coord;
+
+    fn sub(self, other: Coord) -> Coord {
+        Coord {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
